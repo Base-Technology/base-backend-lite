fbbfba68791391957d7e0cf77431ee0fc8b60c44
Lay90 <liqw19@mails.tsinghua.edu.cn>
2023-06-11 10:59:12 +0800

    feat: give bonus for referrer when registering

--- ctrl/handler/chat/utils.go
+++ ctrl/handler/chat/utils.go
@@ -1,9 +1,11 @@
 package chat
 
 import (
-	//"fmt"
+	"fmt"
+	"time"
 
 	"github.com/Base-Technology/base-backend-lite/database"
+	"github.com/Base-Technology/base-backend-lite/seelog"
 )
 
 func max(a, b int) int {
@@ -16,7 +18,7 @@ func max(a, b int) int {
 func updateBalance(limit *database.ChatGPTLimit, response *string) {
 	// TODO: use tokenizer from openai to count token
 	tokenCount := len(*response)
-	//fmt.Println("tokenCount: ", tokenCount)
+	seelog.Infof("tokenCount: %d", tokenCount)
 	limit.DailyLeftCallCount = max(0, limit.DailyLeftCallCount-1)
 	limit.DailyLeftTokenCount = max(0, limit.DailyLeftTokenCount-tokenCount)
 	limit.TotalTokenLeftCount = max(0, limit.TotalTokenLeftCount-tokenCount)
@@ -29,4 +31,22 @@ func enoughBalance(limit *database.ChatGPTLimit, query *string) bool {
 		return false
 	}
 	return true
+}
+
+// increase balance for referer
+func IncreaseBalanceForReferer(referrer_id uint) {
+	limit := &database.ChatGPTLimit{}
+	if err := database.GetInstance().Where(database.ChatGPTLimit{UserID: referrer_id}).First(&limit).Error; err != nil {
+		msg := fmt.Sprintf("IncreaseBalanceForReferer: UserId=%v not found [%v]", referrer_id, err)
+		seelog.Errorf(msg)
+		return
+	}
+	// increase maximum by 25% & refresh daily limit
+	limit.MaxDailyCallCount += 5
+	limit.MaxDailyTokenCount += 3000
+	limit.TotalTokenLeftCount += 15000
+	limit.DailyLeftCallCount = limit.MaxDailyCallCount
+	limit.DailyLeftTokenCount = limit.MaxDailyTokenCount
+	limit.LastResetTime = time.Now()
+	database.GetInstance().Save(limit)
 }
