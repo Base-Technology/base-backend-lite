a5988a2bb5ab6af5930e40ac49dfa97812436f38
bruden <lulargehead@gmail.com>
2023-06-20 23:02:11 +0800

    Update the imtp group id.

--- school/service.go
+++ school/service.go
@@ -1,35 +1,10 @@
 package school
 
 import (
-	"encoding/json"
-	"io/ioutil"
-
 	"github.com/Base-Technology/base-backend-lite/conf"
 	"github.com/Base-Technology/base-backend-lite/imtp"
 	"github.com/pkg/errors"
 )
-
-const (
-	schoolConfigFile = "config/school.json"
-)
-
-var schools map[string]*School
-
-func InitSchoolGroup() error {
-	b, err := ioutil.ReadFile(schoolConfigFile)
-	if err != nil {
-		return err
-	}
-	schoolList := []*School{}
-	if err := json.Unmarshal(b, &schoolList); err != nil {
-		return err
-	}
-	schools = make(map[string]*School)
-	for _, s := range schoolList {
-		schools[s.Name] = s
-	}
-	return nil
-}
 
 func InviteUserToSchoolGroup(address, name string) error {
 	school, ok := schools[name]
